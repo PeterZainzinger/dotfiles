2268af07f85835c65946f1b25fb6b29ea0f9b59b
Peter Zainzinger <peterzainzinger@gmail.com>
2018-01-26 14:24:48 +0100

    add new tmux config

--- setup.sh
+++ setup.sh
@@ -2,27 +2,13 @@
 
 Dotfiles=~/dotfiles
 
-echo "install vim tmux zsh git"
-yes | sudo apt-get install vim tmux zsh git cmake> /dev/null
-
 echo "configure vim"
 cd $HOME
 
-rm -rf .vimrc .gitconfig .zshrc .tmux.conf .zprofile
+rm -rf .gitconfig .zshrc .tmux.conf .zprofile
 
-ln -s $Dotfiles/.vimrc .vimrc
-ln -sr  $Dotfiles/.vim .vim
 ln -s $Dotfiles/.gitconfig .gitconfig
 ln -s $Dotfiles/.tmux.conf .tmux.conf
 ln -s $Dotfiles/.tmux.conf.local .tmux.conf.local
 ln -s $Dotfiles/.zshrc .zshrc
 ln -s $Dotfiles/.zprofile .zprofile
-
-mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-
-wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sh
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
-vim +PluginInstall
-
