a977bd691679fc3281a6ff58e74dfb1c4858b1fc
Peter Zainzinger <peterzainzinger@gmail.com>
2018-01-27 14:27:08 +0100

    fix one more hotkey

--- rc.lua
+++ rc.lua
@@ -525,7 +525,7 @@ globalkeys = gears.table.join(
     awful.key({ modkey }, "r", function () awful.screen.focused().mypromptbox:run() end,
               {description = "run prompt", group = "launcher"}),
 
-    awful.key({ modkey }, "h",
+    awful.key({ modkey }, "m",
               function ()
                   awful.prompt.run {
                     prompt       = "Run Lua code: ",
